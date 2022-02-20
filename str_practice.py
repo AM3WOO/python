37cd9d0854b149745bb6a0bcf6be3b691909ebdf
AM3WOO <96781601+AM3WOO@users.noreply.github.com>
2022-02-20 18:20:37 +0900

    Add files via upload

---
+++ str_practice.py
@@ -0,0 +1,14 @@
+#index
+print("안녕하세요"[0]) #안 (indexing)
+print("안녕하세요"[-5]) #안 (indexing)
+print("안녕하세요"[0:3]) #안녕하 (slicing)
+print("안녕하세요"[:3]) #안녕하 (slicing)
+print("안녕하세요"[3:]) #세요 (slicing)
+
+#slicing 처리를 해도 원본은 변하지 않음
+hello = "안녕하세요"
+print(hello[0:2]) #안녕
+print(hello) #안녕하세요
+
+#len (문자열의 길이)
+print(len("안녕하세요"))
